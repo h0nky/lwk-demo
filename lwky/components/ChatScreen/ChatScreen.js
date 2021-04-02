73bdec4ca847a776345b0a70bf60e2680660918e
Eugene Golovnov <eugene.golovnov@whykingz.com>
2021-04-02 18:22:04 +0300

    Poll WIP / add navigation / add basic routes and components templates

---
+++ lwky/components/ChatScreen/ChatScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const ChatScreen = ({navigation}) => {
+  return (
+    <View>
+      <Text>Chat Screen</Text>
+    </View>
+  );
+};
+
+export default ChatScreen;
---
+++ lwky/constants.ts
@@ -0,0 +1,7 @@
+export const CINDER = '#14131B';
+export const WHITE = '#FFFFFF';
+export const BLUE_RIBBON = '#2467F4';
+export const WATERLOO = '#7E7A9A';
+export const STEEL_GRAY = '#1C1A2A';
+
+// background-image: linear-gradient(to right bottom, #9e397b, #76316f, #50285f, #2d1e4b, #0e1234);
